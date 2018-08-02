fff2bbe3ec982f3a26aa3550190fd3bf4c1d1774
Qianhao <qi_an_hao@126.com>
2018-08-02 14:27:37 +0800

    答辩成果

---
+++ app/src/main/java/com/example/swjtu/market3Activity.java
@@ -0,0 +1,21 @@
+package com.example.swjtu;
+
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class market3Activity extends AppCompatActivity {
+
+    CollapsingToolbarLayout collapsingToolbarLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_market3);
+        Toolbar toolbar=(Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsingToolbarLayout);
+        collapsingToolbarLayout.setTitle("猫");
+    }
+}
